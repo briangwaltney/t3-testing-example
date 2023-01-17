a6426e995970bf839d0f22456563c4d9fa5fb6a6
Brian Gwaltney <brian.gwaltney@gmail.com>
2023-01-17 19:52:49 +0300

    typo

--- src/components/WithTrpcHook.tsx
+++ src/components/WithTrpcHook.tsx
@@ -1,8 +1,8 @@
-import { useAuthNoteRequiredQuery } from "@/hooks/useAuthNotRequiredQuery";
+import { useAuthNotRequiredQuery } from "@/hooks/useAuthNotRequiredQuery";
 import React from "react";
 
 export default function WithTrpcHook() {
-  const { data } = useAuthNoteRequiredQuery();
+  const { data } = useAuthNotRequiredQuery();
 
   return <div>{data && <div>{data.greeting}</div>}</div>;
 }
