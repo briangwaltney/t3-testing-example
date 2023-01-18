7962f2bdbe23df9f2247ed5a4ccd579f33a6a583
Brian Gwaltney <brian.gwaltney@gmail.com>
2023-01-18 17:39:37 +0300

    removed fragile test

--- src/hooks/useProtectedRouteQuery.test.tsx
+++ src/hooks/useProtectedRouteQuery.test.tsx
@@ -1,7 +1,6 @@
 import { useProtectedRouteQuery } from "@/hooks/useProtectedRouteQuery";
 import { createCtx, createUser, resetDb } from "@/utils/testUtils";
 import { cleanup, hookWrapper, renderHook, waitFor } from "@/utils/testWrapper";
-// import { renderHook, waitFor } from "@testing-library/react";
 import cuid from "cuid";
 
 
@@ -20,16 +19,6 @@ describe("useProtectedRouteQuery", () => {
     await waitFor(() => expect(result.current.isSuccess).toBe(false));
     expect(result.current.data).toBe(undefined);
   });
-  test("should return data if logged in", async () => {
-    const { user } = await createUser();
-    const { result } = renderHook(useProtectedRouteQuery, {
-      wrapper: hookWrapper(user),
-    });
-
-    await waitFor(() => expect(result.current.data).not.toBe(undefined));
-    expect(result.current.data).not.toBe(undefined);
-  });
-
   test("should return all users", async () => {
     const { user } = await createUser();
     const ctx = createCtx();
@@ -45,6 +34,6 @@ describe("useProtectedRouteQuery", () => {
     await waitFor(() => expect(result.current.isSuccess).toBe(true));
     await waitFor(() => expect(result.current.data?.length).toBeGreaterThan(0));
 
-    expect(result.current.data?.length).toBeGreaterThan(0);
+    expect(result.current.data?.length).toBe(2);
   });
 });
